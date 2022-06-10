fdcc63a1dd99acd9970147d7bffeef0aeafaa4d6
Reza Pasalar <rezapasalar@yahoo.com>
2022-06-10 23:04:36 +0430

    The _document.js file was added to the pages folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
